d1466820ab3f2bfb55457ccf50c8dfd2f9434dda
Benjamin Hennies <benjaminhennies@posteo.de>
2024-08-26 22:10:39 +0200

    simple duplicate detection and tests

--- solver.h
+++ solver.h
@@ -5,7 +5,7 @@
 #ifndef JUMP_SOLVER_H
 #define JUMP_SOLVER_H
 
-#include <queue>
+#include <deque>
 #include "board.h"
 
 Board importInitialBoard(const std::string& inputFile) {
@@ -13,32 +13,64 @@ Board importInitialBoard(const std::string& inputFile) {
     return initialBoard;
 }
 
-bool findSolution(Board initialBoard) {
-    std::queue<BoardStatus> toProcess;
-    toProcess.emplace(initialBoard);
-    int iterations = 0;
-    while (!toProcess.empty()) {
-        ++iterations;
-        BoardStatus currentBoardStatus = toProcess.front();
-        Board currentBoard = currentBoardStatus.board;
-        toProcess.pop();
-        std::vector<Turn> possible_turns = currentBoard.getAllPossibleTurns();
-        for (Turn current_turn: possible_turns) {
-            BoardStatus newBoardStatus(currentBoardStatus);
-            newBoardStatus.applyTurn(current_turn);
-            if (newBoardStatus.board.countTokens() == 1) {
-                newBoardStatus.print();
-                // finished calculation, clear queue
-                while (!toProcess.empty()) {
-                    toProcess.pop();
-                }
-            } else {
-                toProcess.push(newBoardStatus);
-                if (iterations % 100 == 0) {
-                    newBoardStatus.print();
-                }
+bool boardAlreadyInQueue(const Board& board, const std::deque<BoardStatus>& queue) {
+    int targetTokenCount = board.countTokens(); // Count the number of tokens on the board
+
+    // Iterate from back to front
+    for (auto it = queue.rbegin(); it != queue.rend(); ++it) {
+        int currentTokenCount = it->board.countTokens(); // Count the tokens in the current BoardStatus
+
+        // Break if a BoardStatus has more tokens than the target board
+        if (currentTokenCount > targetTokenCount) {
+            break;
+        }
+
+        // Check if the boards are equivalent
+        if (it->board == board) {
+            return true; // The board is already in the queue
+        }
+    }
+
+    return false; // The board is not in the queue
+}
+
+/**
+ * Pops the first element of the queue and adds all possible next statuses at the back of the queue.
+ * @param toProcess
+ */
+void processNextBoardStatus(std::deque<BoardStatus>& toProcess) {
+    BoardStatus currentBoardStatus = toProcess.front();
+    Board currentBoard = currentBoardStatus.board;
+    toProcess.pop_front();
+    std::vector<Turn> possible_turns = currentBoard.getAllPossibleTurns();
+    for (Turn current_turn: possible_turns) {
+        BoardStatus newBoardStatus(currentBoardStatus);
+        newBoardStatus.applyTurn(current_turn);
+        if (newBoardStatus.board.countTokens() == 1) {
+            newBoardStatus.print();
+            // finished calculation, clear queue
+            while (!toProcess.empty()) {
+                toProcess.pop_front();
+            }
+        } else {
+            if (!boardAlreadyInQueue(newBoardStatus.board, toProcess)) {
+                toProcess.push_back(newBoardStatus);
             }
         }
+    }
+}
+
+bool findSolution(const Board& initialBoard) {
+    std::deque<BoardStatus> toProcess;
+    toProcess.emplace_back(initialBoard);
+    int iterations = 0;
+    while (!toProcess.empty()) {
+        processNextBoardStatus(toProcess);
+        ++iterations;
+        if (iterations % 100 == 0) {
+            toProcess.back().print();
+        }
+
     }
     return true;
 }
--- test.cpp
+++ test.cpp
@@ -5,8 +5,30 @@
 #include <catch2/catch_test_macros.hpp>
 #include "solver.h"
 
+TEST_CASE("One Step Simple Board") {
+    Board testBoard = importInitialBoard("../testData/test_board.csv");
+    std::deque<BoardStatus> toProcess;
+    toProcess.emplace_back(testBoard);
+    processNextBoardStatus(toProcess);
+    // If test fails, check if the boards are computed in another order
+    CHECK(toProcess[0].board == importInitialBoard("../testData/test_board_step_1_1.csv"));
+    CHECK(toProcess[1].board == importInitialBoard("../testData/test_board_step_1_2.csv"));
+}
+
 TEST_CASE("Test Simple Board") {
-    Board testBoard = importInitialBoard("../test_board.csv");
+    Board testBoard = importInitialBoard("../testData/test_board.csv");
     bool found_solution = findSolution(testBoard);
     CHECK(found_solution);
 }
+
+TEST_CASE("Duplicate Detection") {
+    Board testBoard = importInitialBoard("../testData/test_duplicate_detection.csv");
+    std::deque<BoardStatus> toProcess;
+    toProcess.emplace_back(testBoard);
+    processNextBoardStatus(toProcess);
+    processNextBoardStatus(toProcess);
+    processNextBoardStatus(toProcess);
+    CHECK(toProcess.size() == 1);
+    CHECK(toProcess[0].board == importInitialBoard("../testData/test_duplicate_detection_result.csv"));
+}
+
